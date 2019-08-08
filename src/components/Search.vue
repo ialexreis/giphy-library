05b6409647ec1e0f155fe55f545bde9342ce0217
Alexandre Reis <alexandrereis@loba.pt>
2019-08-08 17:50:33 +0100

    Fixed position on Search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <a @click="alert">click me</a> -->
-    <form id="content" class="absolute bottom-0 right-0 z-50 -mr-32">
+    <form id="content" class="fixed bottom-0 right-0 z-50 -mr-32">
         <input type="text" name="input" class="input" id="search-input"  :class="toggleInput">
         <button type="reset" class="search" id="search-btn"  @click="toggleState" :class="toggleButton"></button>
     </form>
