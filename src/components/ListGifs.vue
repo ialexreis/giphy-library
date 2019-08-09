005a10b609549fbec4fa51cc6add057ab698e153
Alexandre Reis <alexandrereis@loba.pt>
2019-08-09 11:26:40 +0100

    Update image label
    w/ sharable links

--- src/components/ListGifs.vue
+++ src/components/ListGifs.vue
@@ -1,9 +1,17 @@
 <template v-if="gifs">
-  <div class="flex flex-wrap -mx-3 overflow-hidden sm:-mx-2 md:-mx-1 lg:-mx-px xl:-mx-2 float-right">
+  <div class="flex flex-wrap -mx-3 overflow-hidden sm:-mx-2 md:-mx-1 lg:-mx-px xl:-mx-2 float-right container_margin">
     <div class="my-3 px-3 w-1/3 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-1 md:px-1 md:w-1/3 lg:my-px lg:px-px lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/4" v-for="gif in gifs" :key="gif">
-      <a :href="gif.url">
-        <img class="gif-image" :src="gif.images.original.url" />
-      </a>
+      <figure class="gif-figure red" @mouseleave="toggleHover" @mouseenter="toggleHover" :class="unHover">
+        <img class="object-cover gif-image" :src="gif.images.original.url" alt="sample28" />
+        <h3 v-if="gif.user">{{ gif.user.display_name }}</h3>
+        <div class="icons">
+          <a :href="`${facebookShare}${gif.images.original.url}`" target="_blank"><span class="icon-facebook-logo"></span></a>
+          <a :href="`${linkedinShare}${gif.images.original.url}`" target="_blank"><span class="icon-linkedin-logo"></span></a>
+          <a :href="`${twitterShare}${gif.images.original.url}`" target="_blank"><span class="icon-twitter"></span></a>
+          <a :href="`${googleShare}${gif.images.original.url}`" target="_blank"><span class="icon-google-plus"></span></a>
+        </div>
+        <div class="corner"><span class="icon-plus"></span></div>
+      </figure>
     </div>
   </div>
 
@@ -17,9 +25,14 @@ export default {
 
   data() {
     return {
+      isHover: false,
       gifs: null,
       apiURL: process.env.VUE_APP_GIPHY_URL,
-      apiKey: process.env.VUE_APP_API_KEY
+      apiKey: process.env.VUE_APP_API_KEY,
+      facebookShare: process.env.VUE_APP_FACEBOOK,
+      linkedinShare: process.env.VUE_APP_LINKEDIN,
+      googleShare: process.env.VUE_APP_GOOGLE,
+      twitterShare: process.env.VUE_APP_TWITTER 
     };
   },
 
@@ -29,6 +42,17 @@ export default {
       fetch(url)
         .then(response => response.json())
         .then(data => (this.gifs = data.data));
+    },
+    toggleHover: function() {
+        this.isHover = !this.isHover
+    },
+  },
+
+  computed: {
+     unHover: function() {
+      return {
+        'hover': !this.isHover
+      }
     }
   },
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/main.css'
+import '@/assets/css/style.css'
 
 
 Vue.config.productionTip = false
